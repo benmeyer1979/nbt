fff7812052f6f45660aed17d459d9f052220a459
Benjamin Meyer <mey1b@nr-linux-01.klinik.uni-mainz.de>
2023-03-01 14:12:44 +0100

    anonymization only for Name and Date of Birth

--- nbt_anonymizer.py
+++ nbt_anonymizer.py
@@ -40,10 +40,20 @@ def nbt_anonymize():
         sysstr = "mkdir -p " + outdir
         os.system(sysstr)
 
-        print(indir + "    " + outdir)
 
-        sysstr = "dicom-anonymizer " + indir + " " + outdir
-        os.system(sysstr)
+        for filename in os.listdir(indir):
+            ds = pydicom.dcmread(indir + "/" + filename)
+            ds.PatientID = subID
+            ds[0x10,0x10].value = 'xxx^xxx^xxx^xxx'
+            ds[0x10,0x20].value = subID
+            ds[0x10,0x30].value = '10090909'
+            print('Writing pseudonymized file to ' + outdir + "/" + filename + "...")
+            ds.save_as(outdir + '/' + filename)
+            print("...done")
+            
+
+#       sysstr = "dicom-anonymizer " + indir + " " + outdir + " --keepPrivateTags -t '(0x0010,0x0010)' empty -t '(0x0010,0x0030)' empty -t '(0x0010,0x0040)' empty"
+#        os.system(sysstr)
       #  pdb.set_trace()
 
 if __name__ == "__main__":
